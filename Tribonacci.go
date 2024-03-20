cde835d605fc21ada8131c8de7ed189b7ebfae62
Eduardo Serra Pierre Vidal <126252966+eduspv@users.noreply.github.com>
2024-03-20 15:15:20 -0300

    Update Tribonacci.go

--- Tribonacci.go
+++ Tribonacci.go
@@ -1,1 +1,28 @@
-package projects_eduardo_serra
+package main
+
+import "fmt"
+
+func Tribonacci(lista []int, n int) []int {
+	if n <= 3 {
+		return lista[:n]
+	}
+
+	for iAtual := 3; iAtual < n; iAtual++ {
+		nextI := lista[iAtual-3] + lista[iAtual-2] + lista[iAtual-1]
+
+		lista = append(lista, nextI)
+	}
+
+	return lista
+}
+
+func main() {
+	var n1 int
+	fmt.Println("Digite o número de vezes para o Tribonacci rodar:")
+	fmt.Scan(&n1)
+
+	listaNumeros := []int{1, 1, 2}
+	resultado := Tribonacci(listaNumeros, n1)
+
+	fmt.Println("Resultado Tribonacci:", resultado)
+}
